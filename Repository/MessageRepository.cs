a8b2d6459ef229ed3525a4f62a296775ccc868fc
Ahmed Sanad <ahmedsanad880@gmail.com>
2024-12-04 21:33:52 +0200

    intialCommit

---
+++ Repository/MessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+public class MessageRepository 
+{
+    public async Task SaveMessageAsync(string user, string message)
+    {
+        // Code to save the message in the database
+        // Example using Entity Framework:
+        // var chatMessage = new ChatMessage { User = user, Message = message };
+        // _dbContext.ChatMessages.Add(chatMessage);
+        // await _dbContext.SaveChangesAsync();
+        await Task.CompletedTask; // Simulate async operat
+    }
+}
