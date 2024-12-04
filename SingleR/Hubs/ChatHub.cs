c5c5ac18b9d66d69f19a0eb1201ec4c2f9c83437
Ahmed Sanad <ahmedsanad880@gmail.com>
2024-12-04 22:10:33 +0200

    chatBox

--- SingleR/Hubs/ChatHub.cs
+++ SingleR/Hubs/ChatHub.cs
@@ -3,18 +3,9 @@ using System.Threading.Tasks;
 
 public class ChatHub : Hub
 {
-    private readonly MessageRepository _messageRepository;
-
-    public ChatHub(MessageRepository messageRepository)
-    {
-        _messageRepository = messageRepository;
-    }
-
+ 
     public async Task SendMessage(string user, string message)
     {
-        // Save the message to the database
-        //await _messageRepository.SaveMessageAsync(user, message);
-
         // Broadcast the message to all clients
         await Clients.All.SendAsync("ReceiveMessage", user, message);
     }
--- obj/Debug/net8.0/ChatApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChatApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b2d6459ef229ed3525a4f62a296775ccc868fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
